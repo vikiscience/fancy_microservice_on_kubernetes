dd4a4ccd4b4865dcd8160dae37cebc48182b7dcf
vikiscience <victoria.bibaeva@yandex.com>
2020-07-19 17:48:59 +0200

    complete upload_docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+export DOCKERPATH=bitelds/demos
+export TAG=fancy_app
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $DOCKERPATH"
+docker tag $TAG $DOCKERPATH:$TAG
 
 # Step 3:
 # Push image to a docker repository
+docker push $DOCKERPATH:$TAG
