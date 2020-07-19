affb26e2e12128ee542c7681f363b48f38db2a25
vikiscience <victoria.bibaeva@yandex.com>
2020-07-19 17:07:18 +0200

    completed shell script to run docker commands

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
+export PORT=8080
+exort TAG=fancy_app
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --no-cache --rm -t $TAG .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -i -t --rm -p $PORT:$PORT $TAG
