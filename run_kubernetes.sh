20b72517a507c4ca8d0f1ee0c739939048880072
vikiscience <victoria.bibaeva@yandex.com>
2020-07-19 20:08:24 +0200

    completed kubernetes shell script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-
-# This tags and uploads an image to Docker Hub
+# This file deploys the Docker image to Kubernetes Cluster
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export DOCKERPATH=bitelds/demos
+export TAG=fancy_app
+export DEPLOYMENT_NAME=my-deployment
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $DEPLOYMENT_NAME --image=$DOCKERPATH:$TAG
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment $DEPLOYMENT_NAME --type=NodePort --port=8080
